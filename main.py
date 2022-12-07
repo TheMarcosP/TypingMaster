ceb290a1a24254b53807babab9d5d15523ebb44d
TheMarcosP <marcosspiotto@gmail.com>
2022-12-07 00:27:45 -0300

    first commit

---
+++ main.py
@@ -0,0 +1,25 @@
+import numpy as np
+import cv2
+import pyautogui
+import time
+
+pyautogui.PAUSE = 0.01
+
+def getTextFromImage()-> str:
+    ...
+
+def typeString(string: str):
+    for i in range(len(string)):
+        pyautogui.press(string[i])
+
+def main():
+    print("running script...")
+    time.sleep(3)
+    string = "now that you know how to type with all ten fingers, lets work on your speed and then move on to other keys"
+
+    typeString(string)
+
+
+if __name__ == "__main__":
+    main()
+
